749365ca64d38d56dafdf650eb327f7b5f2e66a2
AMontalva <montalva@uci.edu>
2014-08-25 20:16:56 -0700

    working timer, changed css

--- js/app.js
+++ js/app.js
@@ -5,38 +5,77 @@
     app.controller("CountController", function($scope) {
 
         // $scope.count = 1500000;
-        $scope.count = 120000; 
+        $scope.count = 60000; 
 
         $scope.min = 60000;
 
-        $scope.minuteDisplay = 2;
+        $scope.minDefault = $scope.count/$scope.min;
 
-        $scope.secondDisplay;
+        $scope.secDisplay = "00";
 
-        $scope.increment = function() {
-            setInterval(function() {
-                $scope.count = $scope.count - 1000;
-                
+        $scope.timer;
 
+        $scope.minDisplay = $scope.minDefault;
+
+        // stops clock when it reaches 0:00
+        $scope.end = function() {
+            clearInterval($scope.timer);
+        };
+
+        // start function
+        $scope.start = function() {
+
+            $scope.timer = setInterval(function() {
+
+                // decrement the total time by one second
+                $scope.count = $scope.count - 10000;
+
+                // starts off minute display
+                if($scope.minDisplay == $scope.minDefault) {
+                    $scope.minDisplay = $scope.minDisplay - 1;
+                }
+
+                // display minute
                 if($scope.count % $scope.min === 0) {
-                    $scope.minuteDisplay = $scope.count / $scope.min;
+                    $scope.minDisplay = $scope.count / $scope.min;
                 }
 
-                $scope.secondDisplay = Math.floor(($scope.count % $scope.min) / 1000);
+                // display seconds
+                $scope.secDisplay = Math.floor(($scope.count % $scope.min) / 1000);
 
-                if($scope.secondDisplay < 0) {
-                    $scope.secondDisplay = 60;
+                // if seconds are less than 0 set to 60 and if it is set to 60 restart decrement of seconds
+                if($scope.secDisplay < 0) {
+                    $scope.secDisplay = 60;
                 }
-                if($scope.secondDisplay === 60) {
-                    $scope.secondDisplay = Math.floor(($scope.count % $scope.min) / 1000);
+                if($scope.secDisplay === 60) {
+                    $scope.secDisplay = Math.floor(($scope.count % $scope.min) / 1000);
                 }
 
+                // add 0 to seconds less than 10
+                if($scope.secDisplay < 10) {
+                    $scope.secDisplay = "0" + $scope.secDisplay.toString();
+                } 
+
+                if($scope.secDisplay == 0 && $scope.minDisplay == 0) {
+                    $scope.end();
+                }
+
+                // update view
                 $scope.$apply();
 
+                // show data in console
                 console.log("Milliseconds: " + $scope.count);
-                console.log("Minutes: " + $scope.minuteDisplay);
-                console.log("Seconds: " + $scope.secondDisplay);
+                console.log("Minutes: " + $scope.minDisplay);
+                console.log("Seconds: " + $scope.secDisplay);
             },1000);
+        };
+
+        // stop function
+        $scope.stop = function() {
+            clearInterval($scope.timer);
+            $scope.count = 120000; 
+            $scope.minDisplay = $scope.minDefault;
+            $scope.secDisplay = "00";
         };
 
     }); 
